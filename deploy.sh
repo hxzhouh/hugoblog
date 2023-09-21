f3a373ee46934724c3a981b7dc0a2e19cf530603
zhouhui02 <zhouhui02@qianxin.com>
2023-09-21 22:04:59 +0800

    building site 2023年 9月21日 星期四 22时04分59秒 CST

--- deploy.sh
+++ deploy.sh
@@ -24,40 +24,5 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# 推送到gitee  
-git push --set-upstream origin master
-
-# 删除打包文件夹
-#rm -rf public
-# 清理
-#hugo --gc
-# 打包。even 是主题
-#hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# 进入打包文件夹
-#cd public
-
-# Add changes to git.
-
-#git init
-#git add -A
-
-# Commit changes.
-#msg="building site `date`"
-#if [ $# -eq 1 ]
-#  then msg="$1"
-#fi
-#git commit -m "$msg"
-
-# 推送到githu  
-# nusr.github.io 只能使用 master分支
-#git push -f git@github.com:hxzhouh/hxzhouh.github.io.git master
-
-# 回到原文件夹
-#cd ..
-
-# 清理
-#hugo --gc
-
-# 打包。even 是主题
-#hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
+# 推送到github
+git push origin 
